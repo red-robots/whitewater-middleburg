b482f185134e3b47f7d33d860aebcfa49ce7031b
accrane <austincrane@gmail.com>
2022-06-28 12:19:56 -0400

    comments

--- comments.php
+++ comments.php
@@ -21,7 +21,7 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
-	<div class="wrapper"></div>
+	<div class="wrapper">
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
