72f756ad1babdf7fcc6b196284653e86e5dcccc8
accrane <austincrane@gmail.com>
2023-04-17 15:38:56 -0400

    fix links

--- parts/extra-cards.php
+++ parts/extra-cards.php
@@ -28,7 +28,7 @@ if ( have_rows('extra_cards') ) : ?>
 		$xtitle = get_sub_field('x_card_sect_title'); 
 		$content = get_sub_field('content');
 		$slides = get_sub_field("images");
-		$cta = get_sub_field("calls_to_action");
+		$cta = get_sub_field("call_to_action");
 		$ctaIcon = get_sub_field("call_to_action_icon");
 		$rel = 'group-'.$iRel;
 				// echo '<pre>';
@@ -52,16 +52,36 @@ if ( have_rows('extra_cards') ) : ?>
 									<?php echo $content; ?>
 								<?php } ?>
 								<?php if ($cta) { ?>
-									<?php foreach ($cta as $ctab) {
-										
-									?>
 									<div class="buttondiv imgwicon">
-										<a href="<?php echo $ctab['call_to_action']['url']; ?>" target="<?php echo $ctab['call_to_action']['target']; ?>" class="btn-sm">
+										<a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>" class="btn-sm">
 											<?php if($ctaIcon){ ?><img src="<?php echo $ctaIcon['url']; ?>"><?php } ?>
-											<span><?php echo $ctab['call_to_action']['title']; ?></span>
+											<span><?php echo $cta['title']; ?></span>
 										</a>
 									</div>
 								<?php } ?>
+								<?php if ($cta) { ?>
+									<!-- <div class="buttondiv imgwicon">
+										<a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>" class="btn-sm">
+											<?php if($ctaIcon){ ?><img src="<?php echo $ctaIcon['url']; ?>"><?php } ?>
+											<span><?php echo $cta['title']; ?></span>
+										</a>
+									</div> -->
+								<?php } ?>
+								<?php if ($ctas) { 
+
+									foreach( $ctas as $l ) { 
+										?>
+									<div class="buttondiv imgwicon">
+										<a <?php if( $l['call_to_action']['title'] == 'View Rates' ){echo 'id="inline"';} ?> href="<?php echo $l['call_to_action']['url']; ?>" target="<?php echo $l['call_to_action']['target']; ?>" class="btn-sm">
+											<?php if($l){ ?><img src="<?php echo $l['url']; ?>"><?php } ?>
+											<span><?php echo $l['call_to_action']['title']; ?></span>
+										</a>
+									</div>
+								<?php 
+									}
+									} ?>
+								<?php if( $i == 1 ) { ?>
+									
 								<?php } ?>
 							</div>
 						</div>
