a5aea3c4dcb24e45096f2bbe8907e1dfb9cd4f76
accrane <austincrane@gmail.com>
2023-05-17 14:41:34 -0400

    fix

--- parts/race-series-filter-rest.php
+++ parts/race-series-filter-rest.php
@@ -88,7 +88,73 @@ $races = get_race_posts();
 // echo '<pre>';
 // print_r($races);
 // echo '</pre>';
+/*
 
+	Pull Race Posts First
+
+*/
+function get_retreats_posts() {
+  $url = 'https://center.whitewater.org/wp-json/wp/v2/retreat?per_page=99';
+  $response = wp_remote_get($url);
+  if ( is_wp_error( $response ) ) {
+    return false;
+  }
+  $posts = json_decode( wp_remote_retrieve_body( $response ), true );
+
+
+  $retreat_post = array();
+  foreach ($posts as $post) {
+  	if( in_array( 'santee', $post['acf']['eventlocation'] ) ){
+  		$retreat_post[] = array(
+  			'title' => $post['title']['rendered'],
+			'pagelink' => $post['link'],
+			'locationname' => $post['acf']['eventlocation_text'],
+			'event_date' => $event_date,
+			'thumbImage' => array(
+				'url' => $post['acf']['vertical_image']['url']
+				// 'url' => $page['acf']['mobile-banner']['url']
+			),
+		);
+	  //   $retreat_post = array(
+			// 'title' => $post['title']['rendered'],
+			// 'pagelink' => $post['link'],
+			// 'locationname' => $post['acf']['eventlocation_text'],
+			// 'start' => $post['acf']['start_date'],
+			// 'end' => $post['acf']['end_date'],
+			// 'hidePostfromMainPage' => $post['acf']['hidePostfromMainPage'],
+			// 'event_date' => $event_date,
+			// 'short_description' => $short_description,
+			// 'eventStatus' => ( $post['acf']['eventstatus'] ) ? $post['acf']['eventstatus']:'upcoming',
+			// 'thumbImage' => $post['acf']['thumbnail_image'],
+			// // 'thumbImage' => $post['acf']['full_image'],
+			// 'eventlocation' => array(),
+			// 'terms' => array(),
+			// 'loc_terms' => array(),
+	  //   );
+	}
+ //    if (!empty($post['acf']['eventlocation'])) {
+ //    	foreach( $post['acf']['eventlocation'] as $rl ){
+ //    		$retreat_post['eventlocation'][] = array(
+	// 	        'value' => $rl['value'], // Store the location value
+	// 	        'label' => $rl['label'] // Store the location label
+	// 	      );
+ //    	}
+	// }
+
+	// foreach ($retreat_post['eventlocation'] as $location) {
+ //      if ($location['value'] === 'santee') {
+ //        $retreat_posts[] = $retreat_post;
+ //      }
+ //    }
+
+  }
+  return $retreat_post;
+}
+
+$retreats = get_retreats_posts();
+// echo '<pre>';
+// print_r($retreats);
+// echo '</pre>';
 /*
 
 	Pull the Instruction_type terms
@@ -314,8 +380,11 @@ $i=1;
 
 		      <?php 
 		      // merge everythig else
+		      
 		      $finalArray = array_merge( $sorted_events, $terms );
 		      $finalArray = array_merge( $finalArray, $cPages );
+		      $finalArray = array_merge( $finalArray, $retreats );
+
 		      // echo '<pre>';
 		      // print_r($finalArray);
 		      // echo '</pre>';
